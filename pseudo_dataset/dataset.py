d940143f42560c3a7955b1ea9ee067d60edfcbe4
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2025-06-11 19:53:19 +0900

    updated

--- pseudo_dataset/dataset.py
+++ pseudo_dataset/dataset.py
@@ -171,6 +171,9 @@ class VITONHDDataset(Dataset):
         masked_vton_img = np.array(masked_vton_img)
         masked_vton_img=cv2.cvtColor(masked_vton_img, cv2.COLOR_BGR2RGB)
         self.agn=masked_vton_img
+        mask=np.array(mask)
+        mask=mask[:,:,np.newaxis]
+        mask=mask[:,:,[0,0,0]]
         self.agn_mask=np.array(mask)
         self.image=self.person_image
         self.image_densepose=densepose
